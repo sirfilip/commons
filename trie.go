4559f0ba06d9435760a1f6713124b82e3cfc03c1
fkostovski <fkostovski@ebay.com>
2020-10-11 04:07:42 +0200

    Add naive concurrency

--- trie.go
+++ trie.go
@@ -1,12 +1,14 @@
 package commons
 
 import (
+	"context"
 	"strings"
+	"sync"
 )
 
 var trans = map[string]string{
-	" ": "|",
-	"|": " ",
+	" ":    "=||=",
+	"=||=": " ",
 }
 
 type Trie interface {
@@ -77,12 +79,97 @@ func (t *trie) Autocomplete(prefix string, maxResults int) []string {
 	return t.autocomplete(curr, prefix, maxResults)
 }
 
+func (t *trie) AutocompleteConcurrent(prefix string, maxResults int) []string {
+	var suggestions []string
+	var found bool
+
+	if maxResults < 1 {
+		return suggestions
+	}
+
+	curr := t.root
+	letters := strings.Split(prefix, "")
+	for _, letter := range letters {
+		curr, found = t.search(curr, letter, false)
+		if !found {
+			return suggestions
+		}
+	}
+
+	return t.autocompleteConcurrent(curr, prefix, maxResults)
+}
+
+type trieAutocompStack struct {
+	node   *trieNode
+	prefix string
+}
+
+func (t *trie) autocompleteConcurrent(node *trieNode, prefix string, maxResults int) []string {
+	var suggestions []string
+	var wg sync.WaitGroup
+	stack := make(chan *trieAutocompStack)
+	word := make(chan string)
+	ctx, quit := context.WithCancel(context.Background())
+	defer quit()
+	for i := 0; i < 4; i++ {
+		go func(ctx context.Context, stack chan *trieAutocompStack, word chan<- string, wg *sync.WaitGroup) {
+			for {
+				select {
+				case <-ctx.Done():
+					return
+				case curr := <-stack:
+					for letter, node := range curr.node.memory {
+						if letter == "*" {
+							word <- curr.prefix
+						} else {
+							if translation, ok := trans[letter]; ok {
+								letter = translation
+							}
+							wg.Add(1)
+							go func(stack chan *trieAutocompStack, curr *trieAutocompStack, node *trieNode, letter string) {
+								stack <- &trieAutocompStack{
+									node:   node,
+									prefix: curr.prefix + letter,
+								}
+							}(stack, curr, node, letter)
+						}
+					}
+					wg.Done()
+				}
+			}
+		}(ctx, stack, word, &wg)
+	}
+
+	wg.Add(1)
+	stack <- &trieAutocompStack{
+		node:   node,
+		prefix: prefix,
+	}
+Done:
+	for {
+		select {
+		case suggestion := <-word:
+			suggestions = append(suggestions, suggestion)
+			if len(suggestions) == maxResults {
+				break Done
+			}
+		case <-func(wg *sync.WaitGroup) <-chan struct{} {
+			ch := make(chan struct{})
+			go func() {
+				wg.Wait()
+				close(ch)
+			}()
+			return ch
+		}(&wg):
+			break Done
+		}
+	}
+	return suggestions
+}
+
 func (t *trie) autocomplete(node *trieNode, prefix string, maxResults int) []string {
 	var suggestions []string
-	stack := []struct {
-		node   *trieNode
-		prefix string
-	}{
+	stack := []trieAutocompStack{
 		{
 			node:   node,
 			prefix: prefix,
@@ -103,10 +190,7 @@ SuggestionsSearch:
 			if translation, ok := trans[letter]; ok {
 				letter = translation
 			}
-			stack = append(stack, struct {
-				node   *trieNode
-				prefix string
-			}{
+			stack = append(stack, trieAutocompStack{
 				node:   node,
 				prefix: curr.prefix + letter,
 			})
